02541fe90243056df08185dd05c6c507d203e328
Pawel Glowacz <mistrz9814@gmail.com>
2019-10-04 01:10:46 +0200

    first push

---
+++ TikTakToe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GridSpace[] spacesList;
+    private string playerSide;
+
+    [SerializeField] GameObject gameOverPanel;
+    [SerializeField] TMP_Text gameOverText;
+    int moveCount = 0;
+
+
+    void Awake()
+    {
+        gameOverPanel.SetActive(false);
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+        moveCount = 0;
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < spacesList.Length; i++)
+        {
+            spacesList[i].SetGameControllerReference(this);
+
+        }
+    }
+
+    public void ResetGame()
+    {
+        moveCount = 0;
+        playerSide = "X";
+        for (int i = 0; i < spacesList.Length; i++)
+        {
+            spacesList[i].ResetSpace();
+        }
+        gameOverPanel.SetActive(false);
+    }
+
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        moveCount++;
+        for (int i = 0; i < 7; i+=3)
+        {
+            if (spacesList[i].buttonText.text == playerSide && spacesList[i+1].buttonText.text == playerSide && spacesList[i+2].buttonText.text == playerSide)
+            {
+                GameOver();
+                return;
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (spacesList[i].buttonText.text == playerSide && spacesList[i + 3].buttonText.text == playerSide && spacesList[i + 6].buttonText.text == playerSide)
+            {
+                GameOver();
+                return;
+            }
+        }
+
+        if (spacesList[0].buttonText.text == playerSide && spacesList[4].buttonText.text == playerSide && spacesList[8].buttonText.text == playerSide)
+        {
+            GameOver();
+            return;
+        }
+        if (spacesList[2].buttonText.text == playerSide && spacesList[4].buttonText.text == playerSide && spacesList[6].buttonText.text == playerSide)
+        {
+            GameOver();
+            return;
+        }
+
+        if (moveCount >= 9)
+        {
+            GameOverPanelTog("Mamy Remis");
+        }
+
+        ChangeSides();
+    }
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+
+
+    void GameOver()
+    {
+        for (int i = 0; i < spacesList.Length; i++)
+        {
+            spacesList[i].button.interactable = false;
+        }
+
+        GameOverPanelTog(playerSide + " Wygrywa!");
+    }
+
+    void GameOverPanelTog(string msg)
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = msg;
+    }
+}
---
+++ TikTakToe/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public TMP_Text buttonText;
+    public string currentPlayer;
+
+    private GameManager gameController;
+
+    public void SetGameControllerReference(GameManager controller)
+    {
+        gameController = controller;
+    }
+
+    public void ResetSpace()
+    {
+        button.interactable = true;
+        buttonText.text = "";
+    }
+    public void SetSpace()
+    {
+        currentPlayer = gameController.GetPlayerSide();
+        button.interactable = false;
+        buttonText.text = currentPlayer;
+        gameController.EndTurn();
+    }
+}
